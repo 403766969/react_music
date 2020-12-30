d7054f5996c90ae58ba43b9435a9018ae794604f
403766969 <403766969@qq.com>
2020-12-30 21:14:58 +0800

    完成页面顶部选项卡部分AppHeader

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,13 +1,53 @@
 import React, { memo } from 'react'
+
+import { headNavLinks } from '@/services/local-data'
+
 import { NavLink } from 'react-router-dom'
 
+import { Input } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+
+import {
+  AppHeaderWrapper,
+  HeaderLeft,
+  HeaderRight
+} from './style'
+
+const showItem = item => (
+  <li className="channel-item" key={item.title}>
+    {
+      item.isNav
+        ? <NavLink to={item.link}>{item.title}</NavLink>
+        : <a href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
+    }
+  </li>
+)
 
 export default memo(function AppHeader() {
   return (
-    <div>
-      <h2>AppHeader</h2>
-      <NavLink to="/discover">发现音乐</NavLink>
-      <NavLink to="/mine">我的音乐</NavLink>
-    </div>
+    <AppHeaderWrapper>
+      <div className="wrap-v1 content">
+        <HeaderLeft>
+          <h1 className="logo sprite_01">
+            <NavLink to="/">网易云音乐</NavLink>
+          </h1>
+          <ul className="channel-menu">
+            {
+              headNavLinks.map(item => showItem(item))
+            }
+          </ul>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />} />
+          <div className="creator-center">
+            <a href="#/">创作者中心</a>
+          </div>
+          <div className="login">
+            <a href="#/">登录</a>
+          </div>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </AppHeaderWrapper>
   )
 })
---
+++ src/services/local-data.js
@@ -0,0 +1,32 @@
+export const headNavLinks = [
+  {
+    title: '发现音乐',
+    link: '/discover',
+    isNav: true
+  },
+  {
+    title: '我的音乐',
+    link: '/mine',
+    isNav: true
+  },
+  {
+    title: '朋友',
+    link: '/friend',
+    isNav: true
+  },
+  {
+    title: '商城',
+    link: 'https://music.163.com/store/product',
+    isNav: false
+  },
+  {
+    title: '音乐人',
+    link: 'https://music.163.com/st/musician',
+    isNav: false
+  },
+  {
+    title: '下载客户端',
+    link: 'https://music.163.com/#/download',
+    isNav: false
+  }
+]

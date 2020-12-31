4f5ab0ae3c7f75aff2cd187bea8465c27d113eb1
403766969 <403766969@qq.com>
2021-01-01 03:21:45 +0800

    封装网络请求axios

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,17 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 
 import { NavLink } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 
-import {
-  dicoverNavLinks
-} from '@/services/local-data'
+import { dicoverNavLinks } from '@/services/local-data'
+import services from '@/services'
 
 import {
   DiscoverWrapper
 } from './style'
 
 export default memo(function Discover(props) {
+
+  useEffect(() => {
+    async function getData() {
+      const res = await services.get('/banner')
+      console.log(res)
+    }
+    getData()
+    return () => console.log('Unmount Discover')
+  }, [])
+
   return (
     <DiscoverWrapper>
       <div className="partition">

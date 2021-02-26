d05b531f3e78443f54ca7698a67c9856614b8fa0
403766969 <403766969@qq.com>
2021-02-26 19:36:31 +0800

    修复歌词解析bug

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -2,19 +2,46 @@ import { NavLink } from 'react-router-dom'
 
 import { emojiUrl } from '@/common/constants'
 
+// export function parseLyric(lyricString) {
+//   const parseExp = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/
+//   const rowArr = lyricString.split('\n')
+//   const lyric = []
+//   for (let row of rowArr) {
+//     if (row) {
+//       const result = parseExp.exec(row)
+//       if (!result) continue
+//       const time1 = result[1] * 60 * 1000
+//       const time2 = result[2] * 1000
+//       const time3 = result[3].length === 3 ? result[3] * 1 : result[3] * 10
+//       const time = time1 + time2 + time3
+//       const content = row.replace(parseExp, '').trim()
+//       const rowObj = { time, content }
+//       lyric.push(rowObj)
+//     }
+//   }
+//   lyric.sort((prev, next) => {
+//     return prev.time - next.time
+//   })
+//   return lyric
+// }
+
 export function parseLyric(lyricString) {
-  const parseExp = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/
+  const regExp = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/
+  const regExpG = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/g
   const rowArr = lyricString.split('\n')
   const lyric = []
   for (let row of rowArr) {
-    if (row) {
-      const result = parseExp.exec(row)
+    if (!row) continue
+    const matchArr = row.match(regExpG)
+    if (!matchArr) continue
+    const content = row.replace(regExpG, '').trim()
+    for (let match of matchArr) {
+      const result = regExp.exec(match)
       if (!result) continue
       const time1 = result[1] * 60 * 1000
       const time2 = result[2] * 1000
       const time3 = result[3].length === 3 ? result[3] * 1 : result[3] * 10
       const time = time1 + time2 + time3
-      const content = row.replace(parseExp, '').trim()
       const rowObj = { time, content }
       lyric.push(rowObj)
     }
